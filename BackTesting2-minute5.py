77dc978e4b4ac5043ee2c19a5ede2461af77f4c3
hong5026 <rong5026@naver.com>
2021-08-02 21:44:13 +0900

    2021-08-02 수정할점 : 산가격과 판가격이 볼린저값으로 들어가서 14.1111123 이렇게 되는거 고침

--- BackTesting2-minute5.py
+++ BackTesting2-minute5.py
@@ -1,4 +1,6 @@
 # 볼린저밴드의 하단값에 닿으면 매수, 2프로 수익을 내거나 볼린저배드 상단에 닿거나, -1.8프로 손실이나면 매도하는 백태스팅
+
+# 2021-08-02 수정할점 : 산가격과 판가격이 볼린저값으로 들어가서 14.1111123 이렇게 되는거 고침
 import pprint
 
 import pyupbit
@@ -10,8 +12,8 @@ from pandas import  DataFrame
 import matplotlib.pyplot as plt
 
 
-cnt = 9000#받아올 데이터 수
-coinlist =["KRW-MLK","KRW-BORA","KRW-XEM","KRW-SC","KRW-BCHA","KRW-ETH","KRW-MED","KRW-BTC","KRW-LSK","KRW-ETC","KRW-REP","KRW-XRP","KRW-FLOW","KRW-DOGE","KRW-STRK","KRW-SXP","KRW-DOT","KRW-UPP","KRW-ANKR","KRW-ELF","KRW-SAND","KRW-BTT"]
+cnt = 90#받아올 데이터 수
+coinlist =["KRW-SC"]
 
 setTime = "minute5"
 
@@ -78,38 +80,46 @@ for j in range(len(coinlist)):
     count = 0  # 거래횟수
     wincount = 0  # 이득인거래수
 
-    target_per = 1.1
+    target_per = 1.02
+    target_sellper = 0.982
     target_buyRSI = 20
     target_sellRSI = 70
 
     for i, row in df.iterrows():
 
-        if row['rsi'] != None and row['rsi'] <= target_buyRSI and buy == False:
+        if row['rsi'] != None and row['low'] <= row['lower'] and buy == False:
             buy = True
             count += 1
-            buyprice =row['lower']
+            buyprice =(int)(row['lower']*10)/10.0
             myasset = 0.9995 * myasset
-            # print("나는 이가격에 샀다 : ",buyprice)
 
 
-        elif row['rsi'] != None and  buy == True and row['high'] >= buyprice*target_per:
+        elif row['rsi'] != None and buy == True and (row['high'] >= buyprice * target_per or
+                                                     row['low'] <= buyprice * target_sellper or
+                                                     row['high'] >= row['upper']):
 
+            if  row['low'] <= buyprice * target_sellper:
 
-            sellprice = buyprice*target_per
+                sellprice = buyprice * target_sellper
 
+            elif row['high'] >= row['upper']:
+                sellprice = (int)(row['upper']*10)/10.0
 
+            else:
+                sellprice = buyprice * target_per
 
             myasset = myasset * (1 + (sellprice - buyprice) / buyprice)
             myasset -= myasset * 0.0005
 
             asset.append(myasset)
-            day.append(str(row.name))
 
             buy = False
             per = per * (1 + (sellprice - buyprice) / buyprice - 0.001)
             if sellprice - buyprice > 0:
                 wincount += 1
-            # print("나는 이가격에 팔았다 : ", row['open'])
+
+            print("Num:", count, "산가격 : ", buyprice, "판가격 : ", sellprice)
+
 
     print("----------------------------------------------┐")
     print("num :",j+1)
@@ -126,7 +136,7 @@ for j in range(len(coinlist)):
     # plt.plot(x_values)
     # plt.show()
 
-    #df.to_excel(coinlist[j]+".xlsx")
+    df.to_excel(coinlist[j]+".xlsx")
 
 
 
